ba9de6274830ea0e42afa7f2429e66bae5a99a8c
MizukiSaitou <r2.f4g.gg9y.rj@gmail.com>
2021-05-23 13:05:13 +0000

    bookの詳細ページ作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,17 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
-  
+
   def show
     @user = User.find(params[:id])
   end
-  
+
   def edit
-   @user = User.current_user
+   user = User.current_user
    redirect_to book_user_path
   end
+  
+  def update
+  end
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_21_083044) do
+ActiveRecord::Schema.define(version: 2021_05_23_074814) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
